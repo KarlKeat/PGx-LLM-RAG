54c6f89aa34d066004a909e5cd2e6749b69b754c
Yidi Huang <yidih@upenn.edu>
2024-04-19 14:29:56 -0400

    Install conda env as jupyter kernel during setup

--- first_time_setup_part1.sh
+++ first_time_setup_part1.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
 conda env create -f pgx_llm_rag.yml
+conda activate pgx_llm_rag
+python -m ipykernel install --user --name=pgx_llm
 echo "run 'conda activate pgx_llm_rag' and then run './first_time_setup_part2.sh"
